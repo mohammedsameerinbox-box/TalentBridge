1ca7c920d0cd05a165f01091310d7933489d5254
Mohammed Sameer <mohammedsameerinbox@gmail.com>
2026-07-07 12:14:49 +0530

    Render deployment

--- app.py
+++ app.py
@@ -1103,9 +1103,6 @@ def notifications():
         "notifications.html",
         notifications=notifications
     )
-
 if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",
-        port=int(os.environ.get("PORT", 8080))
-    )
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
