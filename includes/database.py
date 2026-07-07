d89ac218b6831f22611c4ae5a26cf9f48da40cce
Mohammed Sameer <mohammedsameerinbox@gmail.com>
2026-07-07 21:34:30 +0530

    Updated home page UI

--- includes/database.py
+++ includes/database.py
@@ -1,13 +1,25 @@
 import pymysql
 import os
 
-connection = pymysql.connect(
-    host=os.environ.get("MYSQLHOST"),
-    user=os.environ.get("MYSQLUSER"),
-    password=os.environ.get("MYSQLPASSWORD"),
-    database=os.environ.get("MYSQLDATABASE"),
-    port=int(os.environ.get("MYSQLPORT")),
-    cursorclass=pymysql.cursors.Cursor
-)
+if os.environ.get("MYSQLHOST"):
+    # Render / Railway
+    connection = pymysql.connect(
+        host=os.environ.get("MYSQLHOST"),
+        user=os.environ.get("MYSQLUSER"),
+        password=os.environ.get("MYSQLPASSWORD"),
+        database=os.environ.get("MYSQLDATABASE"),
+        port=int(os.environ.get("MYSQLPORT")),
+        cursorclass=pymysql.cursors.Cursor
+    )
+else:
+    # Local MySQL Workbench
+    connection = pymysql.connect(
+        host="127.0.0.1",
+        user="root",
+        password="root123",
+        database="talentbridge",
+        port=3306,
+        cursorclass=pymysql.cursors.Cursor
+    )
 
 cursor = connection.cursor()
